72dc6c51202cd9bb7f29aa541b3f36d1d5eea390
uwe <uwe.duesing@gmail.com>
2020-05-09 08:58:33 +1200

    added comments

--- projects/mapStuffApp/src/app/app.module.ts
+++ projects/mapStuffApp/src/app/app.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MapStuffModule} from "map-stuff";
+// for development only:
+// import the module directly from the source code to get the latest code changes when developing. The
+// app will recompile when changes in the library are applied
+import {MapStuffModule} from "../../../map-stuff/src/lib/map-stuff.module";
+// importing the module 'node_modules' style is possible, it will however always import the module from the 'dist' folder.
+// That means to see latest changes library needs to be rebuild!
+// import {MapStuffModule} from "map-stuff"
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +18,6 @@ import {MapStuffModule} from "map-stuff";
     BrowserModule,
     AppRoutingModule,
     MapStuffModule,
-    MapStuffModule
   ],
   providers: [],
   bootstrap: [AppComponent]
